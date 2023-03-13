2395f6d48187cec830d41b7744f0e9dfc8ce2999
Maxim Kozhukh <nnicejji@icloud.com>
2023-03-13 05:56:31 +0300

    Updates

--- _app/immutable/chunks/1.8fbedca0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e5d3ccfd.js";export{t as component};
--- _app/immutable/chunks/2.ed543a11.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.94716a37.js";export{t as component};
