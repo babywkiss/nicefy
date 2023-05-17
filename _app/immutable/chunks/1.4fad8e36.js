fa85bdfb65b24e254bcbd43193ecbf3c236a0f1e
wkiskas <nnicejji@icloud.com>
2023-05-17 21:47:02 +0300

    Updates

--- _app/immutable/chunks/1.4fad8e36.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.5ba22077.js";export{t as component};
---
+++ _app/immutable/chunks/3.6079a6eb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/palettes-page.svelte.60fc518b.js";export{t as component};
